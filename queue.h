d4ef5530068352b4bf6de495637d88503a6d65b0
Tiago Rodrigues <tiagorodrigues1590@hotmail.com>
2025-01-27 11:31:14 +0000

    Update queue.h
    Added Comments to the different operation prototypes in queue.h.

--- queue.h
+++ queue.h
@@ -1,8 +1,8 @@
 
 /*******************************************************************************************************
-* NAME:                                                                     
+* NAME: queue.h                                                              
 *                                                                                                       
-* PURPOSE:                                                                      
+* PURPOSE: This file defines the function prototypes for a queue implementation                                                                      
 *                                                                                                       
 * GLOBAL VARIABLES: None                                                                                
 *                                                                                                       
@@ -13,7 +13,9 @@
 * ----------    --------------- ---------       -------         -----------------------------------     
 * 14-01-2025    Tiago Rodrigues                       1         File preparation     
 * 14-01-2025    Tiago Rodrigues                       1         Added prototype functions                                                                                                       
-* 23-01-2025    Tiago Rodrigues                       1         Further changed queue prototypes                                                                                                      
+* 23-01-2025    Tiago Rodrigues                       1         Further changed queue prototypes
+* 27-01-2025    Tiago Rodrigues                       1         Added comments to functions
+*                                                                                                      
 *******************************************************************************************************/
 #ifndef QUEUE_H
 #define QUEUE_H
@@ -65,17 +67,18 @@ extern "C" {
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: create_queue       
 *
-*
+* PURPOSE: Allocates the needed memory for the queue wanted
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
-* 
+* id_of_queue	        void**	        I/O	pointer to the memory position of the queue to implement
+* size_of_datatype      uint64_t        I       byte size of datatype to place in the queue
+* elements_to_allocate  uint64_t        I       number of elements to allocate for the queue
 *
-* RETURNS:
+* RETURNS: void
 *
 *
 *
@@ -86,17 +89,17 @@ void create_queue(void** id_of_queue, uint64_t size_of_datatype, uint64_t elemen
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: check_queue_front
 *
-*
+* PURPOSE: Returns the memory position of the element that is currently on the front of the queue
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
+* id_of_queue   void*	        I	pointer to the memory position of the queue to check
 * 
 *
-* RETURNS:
+* RETURNS: void* (pointer to the memory position of the element at the front of the queue)
 *
 *
 *
@@ -108,17 +111,17 @@ void* check_queue_front(void* id_of_queue);
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: check_queue_back    
 *
-*
+* PURPOSE: Returns the memory position of the element that is currently on the back of the queue
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
+* id_of_queue   void*	        I	pointer to the memory position of the queue to check
 * 
 *
-* RETURNS:
+* RETURNS: void* (pointer to the memory position of the element at the back of the queue)
 *
 *
 *
@@ -128,17 +131,17 @@ void* check_queue_back(void* id_of_queue);
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: queue_pop
 *
-*
+* PURPOSE: Removes one position from the front of the queue
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
-* 
+* id_of_queue   void*	        I	pointer to the memory position of the queue to pop from
 *
-* RETURNS:
+*
+* RETURNS: void
 *
 *
 *
@@ -148,17 +151,18 @@ void queue_pop(void* id_of_queue);
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: queue_push
 *
-*
+* PURPOSE: Pushes an element to the back of the queue
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
-* 
+* id_of_queue   void*	        I	pointer to the memory position of the queue to which the element is being push to
+* data_to_push  void*	        I	pointer to the memory position of the data to push into the queue
 *
-* RETURNS:
+*
+* RETURNS: void
 *
 *
 *
@@ -168,17 +172,17 @@ void queue_push(void* id_of_queue, void* data_to_push);
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: check_queue_is_empty
 *
-*
+* PURPOSE: Checks if the queue is empty or not
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
-* 
+* id_of_queue   void*	        I	pointer to the memory position of the queue to check
 *
-* RETURNS:
+*
+* RETURNS: uint8_t
 *
 *
 *
@@ -189,17 +193,17 @@ uint8_t check_queue_is_empty(void* id_of_queue);
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: check_queue_size
 *
-*
+* PURPOSE: Will return the current element count in the queue
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
-* 
+* id_of_queue   void*	        I	pointer to the memory position of the queue to check
 *
-* RETURNS:
+*
+* RETURNS: uint64_t (size of the queue)
 *
 *
 *
@@ -210,17 +214,17 @@ uint64_t check_queue_size(void* id_of_queue);
 
 /******************************************************************
 *
-* FUNCTION NAME:        
+* FUNCTION NAME: free_queue
 *
-*
+* PURPOSE: Frees the memory allocated for the queue
 *
 * ARGUMENTS:
 *
 * ARGUMENT 	TYPE	        I/O	DESCRIPTION
-* --------	-------------	---	--------------------------
-* 
+* id_of_queue   void*	        I	pointer to the memory position of the queue to free
 *
-* RETURNS:
+*
+* RETURNS: void
 *
 *
 *
